c9e7bba42e64c215e148d44e8b8cb3c68232b3c9
kacy hulme <kacyhulme@gmail.com>
2014-09-01 12:26:20 -0500

    adds filtering and styling to users

--- app/assets/javascripts/controllers/users_controller.js
+++ app/assets/javascripts/controllers/users_controller.js
@@ -1,3 +1,12 @@
 App.UsersController = Ember.ArrayController.extend({
-
+  kids: function() {
+    return this.filter(function(user) {
+      return user.get('age') < 19;
+    });
+  }.property(),
+  adults: function() {
+    return this.filter(function(user) {
+      return user.get('age') > 19;
+    });
+  }.property()
 });
