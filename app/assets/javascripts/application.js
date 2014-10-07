7b5a2ff0adb5525d0a9e1c3cc9928d69e96d8f03
kacy hulme <kacyhulme@gmail.com>
2014-10-07 17:02:26 -0500

    changed application.js in attempt to fixapp not working in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,11 @@
-#= require jquery
-#= require jquery_ujs
-#= require bootstrap
-#= require handlebars
-#= require ember
-#= require ember-data
-#= require_self
-#= require game_of_life
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require game_of_life
 
 window.App = Ember.Application.create({
   LOG_TRANSITIONS: true
